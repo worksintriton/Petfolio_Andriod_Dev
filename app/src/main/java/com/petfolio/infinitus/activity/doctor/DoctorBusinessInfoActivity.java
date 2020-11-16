d434adbea1c2e4f747440055eb56a2d4cbb7eb40
Santhosh Kumar <santhoshvsk94@gmail.com>
2020-11-16 21:07:08 +0530

    Add Business Info in Vetnerian Signup

---
+++ app/src/main/java/com/petfolio/infinitus/activity/doctor/DoctorBusinessInfoActivity.java
@@ -0,0 +1,16 @@
+package com.petfolio.infinitus.activity.doctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.petfolio.infinitus.R;
+
+public class DoctorBusinessInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_business_info);
+    }
+}
