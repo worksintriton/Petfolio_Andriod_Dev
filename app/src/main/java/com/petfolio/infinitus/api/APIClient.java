7bff7d0f55fae19c188ee7a8e21b265466068932
voxitworld <58246208+voxitworld@users.noreply.github.com>
2020-11-18 11:31:37 +0530

    Login,verifty otp , user types functionality completed

---
+++ app/src/main/java/com/petfolio/infinitus/api/APIClient.java
@@ -0,0 +1,38 @@
+package com.petfolio.infinitus.api;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+
+public class APIClient {
+
+
+
+    private static Retrofit retrofit = null;
+    private static OkHttpClient client;
+
+
+    public static String BASE_URL = "http://52.25.163.13:3000/api/";
+
+    public static Retrofit getClient() {
+        client = new OkHttpClient();
+        client = new OkHttpClient.Builder()
+                .readTimeout(3, TimeUnit.MINUTES)
+                .connectTimeout(3, TimeUnit.MINUTES)
+                .writeTimeout(3, TimeUnit.MINUTES )
+                .cache(null)
+                .build();
+                retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        return retrofit;
+    }
+}
