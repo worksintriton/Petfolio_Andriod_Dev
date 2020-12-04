38f3c0adb1994d5675de24a08c2a496954c2bc8a
voxitworld <58246208+voxitworld@users.noreply.github.com>
2020-12-04 15:23:10 +0530

    ui mergered appointment details

--- app/src/main/java/com/petfolio/infinitus/petlover/ManageAddressActivity.java
+++ app/src/main/java/com/petfolio/infinitus/petlover/ManageAddressActivity.java
@@ -2,15 +2,45 @@ package com.petfolio.infinitus.petlover;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.petfolio.infinitus.R;
+import com.wang.avi.AVLoadingIndicatorView;
 
-public class ManageAddressActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class ManageAddressActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.img_back)
+    ImageView img_back;
+
+    @BindView(R.id.avi_indicator)
+    AVLoadingIndicatorView avi_indicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_address);
+        avi_indicator.setVisibility(View.GONE);
+
+        img_back.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.img_back:
+                onBackPressed();
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
--- app/src/main/java/com/petfolio/infinitus/petlover/PetLoverProfileScreenActivity.java
+++ app/src/main/java/com/petfolio/infinitus/petlover/PetLoverProfileScreenActivity.java
@@ -2,18 +2,46 @@ package com.petfolio.infinitus.petlover;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.petfolio.infinitus.R;
+import com.petfolio.infinitus.activity.LoginActivity;
+import com.petfolio.infinitus.sessionmanager.SessionManager;
+
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class PetLoverProfileScreenActivity extends AppCompatActivity {
+public class PetLoverProfileScreenActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.img_back)
     ImageView img_back;
+
+    @BindView(R.id.txt_usrname)
+    TextView txt_usrname;
+
+    @BindView(R.id.txt_mail)
+    TextView txt_mail;
+
+    @BindView(R.id.txt_phn_num)
+    TextView txt_phn_num;
+
+    @BindView(R.id.txt_manage_address)
+    TextView txt_manage_address;
+
+    @BindView(R.id.txt_change_password)
+    TextView txt_change_password;
+
+    @BindView(R.id.txt_logout)
+    TextView txt_logout;
+    private SessionManager session;
+    String name,emailid,phoneNo,userid;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +49,23 @@ public class PetLoverProfileScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pet_lover_profile_screen);
         ButterKnife.bind(this);
 
-        img_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
+
+        session = new SessionManager(getApplicationContext());
+        HashMap<String, String> user = session.getProfileDetails();
+        name = user.get(SessionManager.KEY_FIRST_NAME);
+        emailid = user.get(SessionManager.KEY_EMAIL_ID);
+        phoneNo = user.get(SessionManager.KEY_MOBILE);
+        userid = user.get(SessionManager.KEY_ID);
+
+        txt_usrname.setText(name);
+        txt_mail.setText(emailid);
+        txt_phn_num.setText(phoneNo);
+
+        img_back.setOnClickListener(this);
+        txt_manage_address.setOnClickListener(this);
+        txt_change_password.setOnClickListener(this);
+        txt_logout.setOnClickListener(this);
+
     }
 
     @Override
@@ -34,4 +73,65 @@ public class PetLoverProfileScreenActivity extends AppCompatActivity {
         super.onBackPressed();
         finish();
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.img_back:
+                onBackPressed();
+                break;
+                case R.id.txt_manage_address:
+                    gotoManageAddress();
+                break;
+                case R.id.txt_change_password:
+                break;
+                case R.id.txt_logout:
+                    confirmLogoutDialog();
+                break;
+        }
+    }
+
+    private void gotoManageAddress() {
+        startActivity(new Intent(getApplicationContext(),ManageAddressActivity.class));
+    }
+
+    private void confirmLogoutDialog(){
+
+        android.app.AlertDialog.Builder alertDialogBuilder = new android.app.AlertDialog.Builder(PetLoverProfileScreenActivity.this);
+        alertDialogBuilder.setMessage("Are you sure want to logout?");
+        alertDialogBuilder.setPositiveButton("yes",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1)
+                    {
+
+                        gotoLogout();
+
+
+                    }
+                });
+
+        alertDialogBuilder.setNegativeButton("No",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                alertDialogBuilder.setCancelable(true);
+            }
+        });
+
+        android.app.AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+
+    }
+    private void gotoLogout() {
+        session.logoutUser();
+        session.setIsLogin(false);
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        finish();
+
+
+
+
+    }
+
+
 }
